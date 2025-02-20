aef0af49921b2c7be69e1723314dd270393438a3
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-02-20 06:00:25 +0400

    Add environment configuration and application paths

---
+++ src/config/paths.ts
@@ -0,0 +1,23 @@
+export const paths = {
+  auth: {
+    register: {
+      path: '/auth/register',
+      getHref: (redirectTo?: string | null | undefined) => `/auth/register${redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''}`,
+    },
+    login: {
+      path: '/auth/login',
+      getHref: (redirectTo?: string | null | undefined) => `/auth/login${redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''}`,
+    },
+  },
+
+  platform: {
+    dashboard: {
+      path: '/',
+      getHref: () => '/',
+    },
+    profile: {
+      path: 'profile',
+      getHref: () => '/profile',
+    },
+  },
+} as const;
