74a7b4faa33b63a448e2d4a5214a21be65dd2fec
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-02-20 06:01:41 +0400

    Add authentication and API client libraries

---
+++ src/lib/auth.tsx
@@ -0,0 +1,95 @@
+import { ReactNode } from 'react';
+import { configureAuth } from 'react-query-auth';
+import { Navigate, useLocation } from 'react-router';
+import { z } from 'zod';
+
+import { api } from './api-client';
+
+import { paths } from '@/config/paths';
+import { AuthResponse, User } from '@/types/api';
+
+// api call definitions for auth (types, schemas, requests):
+// these are not part of features as this is a module shared across features
+
+const getUser = async (): Promise<User> => {
+  // const response = await api.get('/auth/me');
+
+  // return response.data;
+  return {
+    id: '1',
+    email: 'test@test.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    role: 'ADMIN',
+    teamId: '1',
+    bio: 'I am a test user',
+    createdAt: 1713542400,
+  };
+};
+
+const logout = (): Promise<void> => {
+  return api.post('/auth/logout');
+};
+
+export const loginInputSchema = z.object({
+  email: z.string().min(1, 'Required').email('Invalid email'),
+  password: z.string().min(5, 'Required'),
+});
+
+export type LoginInput = z.infer<typeof loginInputSchema>;
+const loginWithEmailAndPassword = (data: LoginInput): Promise<AuthResponse> => {
+  return api.post('/auth/login', data);
+};
+
+export const registerInputSchema = z
+  .object({
+    email: z.string().min(1, 'Required'),
+    firstName: z.string().min(1, 'Required'),
+    lastName: z.string().min(1, 'Required'),
+    password: z.string().min(5, 'Required'),
+  })
+  .and(
+    z
+      .object({
+        teamId: z.string().min(1, 'Required'),
+        teamName: z.null().default(null),
+      })
+      .or(
+        z.object({
+          teamName: z.string().min(1, 'Required'),
+          teamId: z.null().default(null),
+        }),
+      ),
+  );
+
+export type RegisterInput = z.infer<typeof registerInputSchema>;
+
+const registerWithEmailAndPassword = (data: RegisterInput): Promise<AuthResponse> => {
+  return api.post('/auth/register', data);
+};
+
+const authConfig = {
+  userFn: getUser,
+  loginFn: async (data: LoginInput) => {
+    const response = await loginWithEmailAndPassword(data);
+    return response.user;
+  },
+  registerFn: async (data: RegisterInput) => {
+    const response = await registerWithEmailAndPassword(data);
+    return response.user;
+  },
+  logoutFn: logout,
+};
+
+export const { useUser, useLogin, useLogout, useRegister, AuthLoader } = configureAuth(authConfig);
+
+export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const user = useUser();
+  const location = useLocation();
+
+  if (!user.data) {
+    return <Navigate to={paths.auth.login.getHref(location.pathname)} replace />;
+  }
+
+  return children;
+};
